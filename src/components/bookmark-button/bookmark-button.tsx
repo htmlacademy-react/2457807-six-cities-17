9f2c319bd0b944b522a9a2995c13f7d729159169
Malyavko Anton <ant_malyavko@mail.ru>
2024-12-29 20:39:55 +0300

    Обработчик добавления в избранное

--- src/components/bookmark-button/bookmark-button.tsx
+++ src/components/bookmark-button/bookmark-button.tsx
@@ -9,8 +9,16 @@ type BookmarkButtonProps = {
 
 function BookmarkButton({bookmarkClass, cardPlace}: BookmarkButtonProps): JSX.Element{
   const isSelectedFavorite = cardPlace?.isFavorite;
+  const handleButtonClick = (cardPlaceFavorite:ListOfferType|undefined):void =>{
+    if(cardPlaceFavorite?.isFavorite) {
+      cardPlaceFavorite.isFavorite = false;
+    }
+    if(!cardPlaceFavorite?.isFavorite === false){
+      cardPlaceFavorite.isFavorite = true;
+    }
+  };
   return (
-    <button className={`${bookmarkClass}__bookmark-button ${isSelectedFavorite ? `${bookmarkClass}__bookmark-button--active` : ''} button`} type="button">
+    <button onClick = {() => handleButtonClick(cardPlace)} className={`${bookmarkClass}__bookmark-button ${isSelectedFavorite ? `${bookmarkClass}__bookmark-button--active` : ''} button`} type="button">
       <svg className={`${bookmarkClass}__bookmark-icon`} width={BookmarkAttributes[bookmarkClass].width} height={BookmarkAttributes[bookmarkClass].height}>
         <use xlinkHref="#icon-bookmark"></use>
       </svg>

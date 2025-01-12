0041baf36e2d10a4c8fdd9aac7db65f9e45e66a9
Malyavko Anton <ant_malyavko@mail.ru>
2025-01-12 16:05:33 +0300

    уберет ошибку типа в рейтинге

--- src/components/rating/rating.tsx
+++ src/components/rating/rating.tsx
@@ -2,14 +2,14 @@ import { RatingAttributes } from '../../style-options';
 
 type RatingProps = {
     ratingClass: string;
-    ratingValue: number;
+    ratingValue: number | null;
   }
 
 function Rating({ratingClass, ratingValue}: RatingProps):JSX.Element{
   return (
     <div className={`${ratingClass}__rating rating`}>
       <div className={`${ratingClass}__stars rating__stars`}>
-        <span style={{width: `${Math.round(ratingValue) * 2 * 10}%` }} />
+        <span style={{width: `${Math.round(ratingValue === null ? 0 : ratingValue) * 2 * 10}%` }} />
         <span className="visually-hidden">Rating</span>
       </div>
       {RatingAttributes[ratingClass].ratingVisibility &&

5e1b7ddc0f2a2675444476693177e00e2015b40a
Malyavko Anton <ant_malyavko@mail.ru>
2025-01-25 18:04:34 +0300

    Подготовка к защите

--- src/components/header-navigation/header-navigation.tsx
+++ src/components/header-navigation/header-navigation.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import { AppRoute, AuthorizationStatus } from '../../constants';
+import { AppRoute, AuthorizationStatus} from '../../constants';
 import { logOutAction } from '../../store/api-actions';
 import { useAppDispatch, useAppSelector } from '../../hooks';
 import { selectFavorites } from '../../store/offers/offers-selectors';
